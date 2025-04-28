fdd3802c6939e36b7acc5ecb8eee94715e520e00
Giovanni Pace <g.pace@braintechinnovations.it>
2025-04-28 12:57:05 +0200

    Aggiunto primo pezzo di correzione

---
+++ its31_lez00_introduzione/its31_lez05_task/Models/Libro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace its31_lez05_task.Models
+{
+    internal class Libro
+    {
+        public string? Titolo { get; set; }
+        public string? Autore { get; set; }
+        public string? Genere { get; set; }
+        public string? Isbn { get; set; }
+        public int? Anno { get; set; }
+        public bool IsDisponibile { get; set; }
+
+        public override string ToString()
+        {
+            return $"Titolo: {Titolo}\n" +
+                $"Autore: {Autore}\n" +
+                $"Anno: {Anno}\n";
+        }
+    }
+}
---
+++ its31_lez00_introduzione/its31_lez05_task/Program.cs
@@ -0,0 +1,29 @@
+﻿using its31_lez05_task.Models;
+
+List<Libro> elenco =  new List<Libro>();
+
+Libro libUno = new()
+{
+    Autore = "JRRT",
+    Titolo = "Il signore degli anelli",
+    Anno = 1922,
+    IsDisponibile = true,
+    Isbn = "AB12345"
+};
+
+Libro libDue = new()
+{
+    Autore = "JRRT",
+    Titolo = "Il signore degli anelli: Le due torri",
+    Anno = 1922,
+    IsDisponibile = true,
+    Isbn = "AB12345"
+};
+
+elenco.Add(libUno);
+elenco.Add(libDue);
+
+for(int i=0; i < elenco.Count; i++)
+{
+    Console.WriteLine(elenco[i]);
+}
